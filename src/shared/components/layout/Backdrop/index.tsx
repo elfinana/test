a15885275dfc04e554995e178604f13f40d9db92
JeongTaekCho <cjt3591@gmail.com>
2024-10-11 22:23:38 +0900

    Design: 모달 컴포넌트 제작

--- src/shared/components/layout/Backdrop/index.tsx
+++ src/shared/components/layout/Backdrop/index.tsx
@@ -8,7 +8,7 @@ export default function Backdrop({ onClick }: BackDropProps) {
   return (
     <motion.div
       onClick={onClick}
-      className="fixed left-0 top-0 z-backdrop h-screen w-full max-w-[600px] bg-black-50"
+      className="layout-center fixed left-0 top-0 z-backdrop h-screen w-full bg-black-50"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
