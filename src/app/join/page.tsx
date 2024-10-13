6c137bedc71235c54caf39039a064aae7533e9fc
JeongTaekCho <cjt3591@gmail.com>
2024-10-13 13:59:55 +0900

    Design: 헤더 컴포넌트 제작

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,3 +1,10 @@
+import Header from '@/shared/components/layout/Header';
+
 export default function JoinPage() {
-  return <div className="w-full">회원가입 페이지</div>;
+  return (
+    <div className="w-full">
+      <Header title="회원가입" />
+      회원가입 페이지
+    </div>
+  );
 }
---
+++ src/shared/components/icons/BackIcon/index.tsx
@@ -0,0 +1,7 @@
+export default function BackIcon() {
+  return (
+    <svg width="11" height="18" viewBox="0 0 11 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M10 1L2 9L10 17" stroke="#27282C" strokeWidth="2" />
+    </svg>
+  );
+}
