648c5d029f433304bf70fb5b4d284c4e0ebacc98
JeongTaekCho <cjt3591@gmail.com>
2024-10-14 17:30:05 +0900

    Feat: 뒤로가기 기능

--- src/shared/components/layout/Header/index.tsx
+++ src/shared/components/layout/Header/index.tsx
@@ -1,17 +1,30 @@
-import BackIcon from '@/shared/components/icons/BackIcon';
+import GoBackButton from '@/shared/components/buttons/GoBackButton';
 import Flex from '@/shared/components/layout/Flex';
 
 interface HeaderProps {
   title: string;
 }
+
+const isWebView = () => {
+  const userAgent = navigator.userAgent || navigator.vendor;
+
+  // Android WebView와 iOS WebView 감지
+  const isAndroidWebView = /\bwv\b/.test(userAgent) || /Android.*Version\/[0-9.]+/.test(userAgent);
+  const isIOSWebView = /\b(iPhone|iPod|iPad).*AppleWebKit(?!.*Safari)/.test(userAgent);
+
+  return isAndroidWebView || isIOSWebView;
+};
+
 export default function Header({ title }: HeaderProps) {
+  if (isWebView()) {
+    return null; // WebView일 경우 헤더를 숨김
+  }
+
   return (
     <header className="w-full bg-white px-20 py-16">
       <Flex justify="between" align="center">
         <div>
-          <button>
-            <BackIcon />
-          </button>
+          <GoBackButton />
         </div>
         <div>
           <h2 className="text-16 font-semibold text-textPrimary">{title}</h2>
